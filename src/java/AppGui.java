0380dc6bfe52bc93d5015c16df6ec918eba971e1
Vyacheslav Lozhnikov <vereskanthem@gmail.com>
2017-05-16 00:05:01 +0600

    Create template of program and now compiling with ANT

---
+++ src/java/AppGui.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class AppGui extends JFrame  {
+
+    public static void main(String[] args)  {
+
+        System.out.println("App compile ans works!");
+
+    };
+
+}
